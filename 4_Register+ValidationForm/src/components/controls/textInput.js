4140e99947ab460d70fb21f6cafd42f085d31f15
irynasavina <irynasavina@gmail.com>
2019-04-06 00:56:39 +0300

    Валидация на клиенте

--- 4_Register+ValidationForm/src/components/controls/textInput.js
+++ 4_Register+ValidationForm/src/components/controls/textInput.js
@@ -5,10 +5,16 @@ import '../style.css';
 export default class textInput extends Component {
     
     render() {
+        let errorMessage = null;
+        if (this.props.state != undefined && this.props.state.isValid == false) {
+            errorMessage = this.props.state.message;
+        }
+        let asterisk = (this.props.required)? (<span className="asterisk">*</span>) : null;
         return (
             <div className="row">
-                <div className="caption">{this.props.caption}:</div>
+                <div className="caption">{this.props.caption}:{asterisk}</div>
                 <div className="control"><input type={this.props.type} value={this.props.value} onChange={this.props.onChangeValue}/></div>
+                <div className="error">{errorMessage}</div>
             </div>
         );
     }
--- 4_Register+ValidationForm/src/components/registerForm.js
+++ 4_Register+ValidationForm/src/components/registerForm.js
@@ -10,7 +10,12 @@ export default class registerForm extends Component {
         name: "",
         password: "",
         confirmPassword: "",
-        email: ""
+        email: "",
+        loginState: { isValid: true, message: "" },
+        nameState: { isValid: true, message: "" },
+        passwordState: { isValid: true, message: "" },
+        confirmPasswordState: { isValid: true, message: "" },
+        emailState: { isValid: true, message: "" },
     }
 
     changeLogin = (event) => {
@@ -33,26 +38,52 @@ export default class registerForm extends Component {
         this.setState({ email: event.target.value });
     }
 
-    clickRegister = async () => {
-        let response = await Ajax.post('register', 'login=' + encodeURIComponent(this.state.login) + '&password=' + encodeURIComponent(this.state.password) +
-            '&email=' + encodeURIComponent(this.state.email) + '&name=' + encodeURIComponent(this.state.name));
-        let data = JSON.parse(response);
-        if (!data.error) {
-            if (this.props.onLogin) {
-                this.props.onLogin(data.roles, data.sessionID, data.message);
-            }
+    requiredField = (field) => {
+        if (field == '') {
+            return { isValid: false, message: "Обязательное поле" };
         }
-        console.log(response);
+        return { isValid: true, message: "" }
     }
 
+    clickRegister = async () => {
+        let validLogin = this.requiredField(this.state.login);
+        let validPassword = this.requiredField(this.state.password);
+        let validConfirmPassword = this.requiredField(this.state.confirmPassword);
+        if (validConfirmPassword.isValid && this.state.password != this.state.confirmPassword) {
+            validConfirmPassword = { isValid: false, message: "Пароль и подтверждение должны совпадать" }
+        }
+        let validEmail = this.requiredField(this.state.email);
+        if (validEmail.isValid) {
+            let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+            console.log(reg.test(this.state.email));
+            if (!reg.test(this.state.email)) {
+                console.log(111);
+                validEmail = { isValid: false, message: "Некорректный e-mail" }
+                console.log(validEmail);
+            }
+        }
+        this.setState({ loginState: validLogin, passwordState: validPassword, confirmPasswordState: validConfirmPassword, emailState: validEmail });
+        let validForm = validLogin.isValid && validPassword.isValid && validConfirmPassword.isValid && validEmail.isValid;
+        if (validForm) {
+            let response = await Ajax.post('register', 'login=' + encodeURIComponent(this.state.login) + '&password=' + encodeURIComponent(this.state.password) +
+                '&email=' + encodeURIComponent(this.state.email) + '&name=' + encodeURIComponent(this.state.name));
+            let data = JSON.parse(response);
+            if (!data.error) {
+                if (this.props.onLogin) {
+                    this.props.onLogin(data.roles, data.sessionID, data.message);
+                }
+            }
+            console.log(response);
+        }
+    }
     render() {
         return (
             <div className="form">
-                <TextInput caption="Логин" type="text" value={this.state.login} onChangeValue={this.changeLogin}/>
-                <TextInput caption="Пароль" type="password" onChangeValue={this.changePassword}/>
-                <TextInput caption="Подтвердите пароль" type="password" onChangeValue={this.changeConfirmPassword}/>
-                <TextInput caption="Имя" type="text" onChangeValue={this.changeName}/>
-                <TextInput caption="E-mail" type="email" onChangeValue={this.changeEmail}/>
+                <TextInput caption="Логин" required={true} state={this.state.loginState} type="text" value={this.state.login} onChangeValue={this.changeLogin}/>
+                <TextInput caption="Пароль" required={true} state={this.state.passwordState} type="password" onChangeValue={this.changePassword}/>
+                <TextInput caption="Подтвердите пароль" required={true}  state={this.state.confirmPasswordState} type="password" onChangeValue={this.changeConfirmPassword}/>
+                <TextInput caption="Имя" state={this.state.nameState} type="text" onChangeValue={this.changeName}/>
+                <TextInput caption="E-mail" state={this.state.emailState} required={true} state={this.state.emailState} type="email" onChangeValue={this.changeEmail}/>
                 <div className="row">
                     <div></div>
                     <div><input type="button" value="Регистрация" onClick={this.clickRegister}/></div>
