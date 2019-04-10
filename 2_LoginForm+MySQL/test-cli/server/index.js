d2e9ec48bb6828c9cb7f3a2d8b4c593679a8f2d3
irynasavina <irynasavina@gmail.com>
2019-04-10 21:16:03 +0300

    Update index.js

--- 2_LoginForm+MySQL/test-cli/server/index.js
+++ 2_LoginForm+MySQL/test-cli/server/index.js
@@ -54,7 +54,5 @@ app.post('/login', async function(req, res) {
 app.listen(app.get('port'),
     function() {
         console.log('Server started: http://localhost:' + app.get('port') + '/');
-        //let data = "do shash'owania";
-        //console.log(crypto.createHash('md5').update(data).digest("hex"));
     }
 );
